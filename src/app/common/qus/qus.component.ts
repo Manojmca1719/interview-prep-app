337d0e47c39cc1df995e45bf1eff6db62d71d3d2
Manojmca1719 <manojmca1719@gmail.com>
2024-01-23 20:54:52 +0530

    qus alone updated

---
+++ src/app/common/qus/qus.component.ts
@@ -0,0 +1,29 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-qus',
+  standalone: true,
+  imports: [],
+  templateUrl: './qus.component.html',
+  styleUrl: './qus.component.scss'
+})
+export class QusComponent {
+
+  @ViewChild('QusModal')
+  public QusModal!: ElementRef;
+
+  public subjectTitle: string = '';
+  public qusData: Array<any> = [];
+
+  public element: any;
+  constructor() { }
+  public openQusModal(subject: any, qus: any) {
+    this.subjectTitle = subject;
+    this.qusData = qus;
+    this.QusModal.nativeElement.classList.add('show');
+  }
+
+  public closeQusModal() {
+    this.QusModal.nativeElement.classList.remove('show');
+  }
+}
--- src/app/topic/javascript/javascript.component.ts
+++ src/app/topic/javascript/javascript.component.ts
@@ -1,16 +1,19 @@
 import { Component, ViewChild } from '@angular/core';
 import { CommonService } from '../../common/common.service';
 import { HttpClientModule } from '@angular/common/http';
+import { QusComponent } from '../../common/qus/qus.component';
 
 @Component({
   selector: 'app-javascript',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, QusComponent],
   templateUrl: './javascript.component.html',
   styleUrl: './javascript.component.scss',
   providers: [CommonService]
 })
 export class JavascriptComponent {
+
+  @ViewChild(QusComponent) QusComponent!: QusComponent;
 
   public dataQusAns: Array<any> = [];
   constructor(private commonService: CommonService) {
@@ -20,6 +23,13 @@ export class JavascriptComponent {
   public getQusAnsJsonData() {
     this.commonService.getData('./assets/jsQus.json').subscribe((data: any) => {
       this.dataQusAns = data;
+      this.dataQusAns.forEach((data:any)=>{
+        data["visibility"] = "ans" in data;
+      })
     })
+  }
+
+  public getQusModal(){
+    this.QusComponent.openQusModal('Javascript', this.dataQusAns);
   }
 }
